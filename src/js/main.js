2befe6a3182a32f54fbdc96bb0661caefd5b3f19
JaedenP2003 <jaeden.platt2003@gmail.com>
2026-01-15 17:34:50 -0700

    Finished NPS 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,24 @@
-import { getParkData } from "./parkService.mjs";
-
+import { getParkData, parkInfoLinks } from "./parkService.mjs";
+import setHeaderFooter from "./setHeaderFooter.mjs";
+import { mediaCardTemplate } from "./templates.mjs";
 const parkData = getParkData();
 
 const disclaimer = document.querySelector(".disclaimer > a");
 disclaimer.href = parkData.url;
 disclaimer.innerHTML = parkData.fullName;
 
-function parkInfoTemplate(info) {
-  return `<a href="/" class="hero-banner__title">${info.name}</a>
-  <p class="hero-banner__subtitle">
-    <span>${info.designation}</span>
-    <span>${info.states}</span>
-  </p>`;
+function setParkIntro(data) {
+  const introEl = document.querySelector(".intro");
+  introEl.innerHTML = `<h1>${parkData.fullName}</h1>
+  <p>${parkData.description}</p>`;
+}
+function setParkInfoLinks(data) {
+  const infoEl = document.querySelector(".info");
+  const html = data.map(mediaCardTemplate);
+  infoEl.innerHTML = html.join("");
 }
 
-document.querySelector("head > title").textContent = parkData.fullName;
+setHeaderFooter(parkData);
+setParkIntro(parkData);
+setParkInfoLinks(parkInfoLinks);
 
-document.querySelector(".hero-banner > img").src = parkData.images[0].url;
-
-document.querySelector(".hero-banner__content").innerHTML =
-  parkInfoTemplate(parkData);
